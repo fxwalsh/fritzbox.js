8cac99035827142e79ef25206987029aeeaee381
fxwalsh <fxwalsh@wit.ie>
2019-02-25 15:31:21 +0000

    my fritx

--- src/inet.js
+++ src/inet.js
@@ -13,7 +13,7 @@ const fritzRequest = require('./request.js')
  * @param  {Object} options - FritzBox.js options object.
  * @return {Array} Returns an array of active calls.
  */
-fritzInet.setUnlimitedStandardAccess = async (options) => {
+fritzInet.setUnlimitedAccess = async (options) => {
   if (!options.sid) {
     options.sid = await fritzLogin.getSessionId(options)
     if (options.sid.error) return options.sid
@@ -21,37 +21,81 @@ fritzInet.setUnlimitedStandardAccess = async (options) => {
   options.removeSidFromUri = true
   const form1 = {
     sid: options.sid,
-    back_to_page:'/internet/kids_profilelist.lua',
-    edit:'filtprof1',
-    time:'unlimited',
+    back_to_page: '/internet/kids_profilelist.lua',
+    edit: options.profile,
+    time: 'unlimited',
     netappschosen: '',
-    choosenetapps:'choose',
-    validate:'apply',
-    xhr:1
+    choosenetapps: 'choose',
+    validate: 'apply',
+    xhr: 1
   }
   const response = await fritzRequest.request('/internet/kids_profileedit.lua', 'POST', options, false, false, form1)
   if (response.error) return response
 
-const form2 = {
+  const form2 = {
     sid: options.sid,
-    back_to_page:'/internet/kids_profilelist.lua',
-    edit:'filtprof1',
-    time:'unlimited',
-    oldpage:'/internet/kids_profileedit.lua',
-    lang:'en',
-    timer_complete:0,
-    no_sidrenew:'',
-    apply:'',
-     netappschosen: '',
-    choosenetapps:'choose',
+    back_to_page: '/internet/kids_profilelist.lua',
+    edit: options.profile,
+    time: 'unlimited',
+    oldpage: '/internet/kids_profileedit.lua',
+    lang: 'en',
+    timer_complete: 0,
+    no_sidrenew: '',
+    apply: '',
+    netappschosen: '',
+    choosenetapps: 'choose',
     //validate:'apply',
-    xhr:1
+    xhr: 1
   }
   const response2 = await fritzRequest.request('/data.lua', 'POST', options, false, false, form2)
   if (response2.error) return response
   //const result = JSON.parse(response.body).data.foncalls.activecalls
-  
   return response2.body
+}
+
+/**
+ * Set no Inet access.
+ * @param  {Object} options - FritzBox.js options object.
+ * @return {Array} Returns an array of active calls.
+ */
+fritzInet.setNeverAccess = async (options) => {
+  if (!options.sid) {
+    options.sid = await fritzLogin.getSessionId(options)
+    if (options.sid.error) return options.sid
+  }
+
+  options.removeSidFromUri = true
+  const form1 = {
+    sid: options.sid,
+    back_to_page: '/internet/kids_profilelist.lua',
+    edit: options.profile,
+    time: 'never',
+    netappschosen: '',
+    choosenetapps: 'choose',
+    validate: 'apply',
+    xhr: 1
+  }
+  const response = await fritzRequest.request('/internet/kids_profileedit.lua', 'POST', options, false, false, form1)
+  if (response.error) return response
+
+  const form2 = {
+    sid: options.sid,
+    back_to_page: ' ',
+    edit: options.profile,
+    time: 'never',
+    oldpage: '/internet/kids_profileedit.lua',
+    lang: 'en',
+    timer_complete: 1,
+    no_sidrenew: '',
+    apply: '',
+    netappschosen: '',
+    choosenetapps: 'choose',
+    xhr: 1
+  }
+  const response2 = await fritzRequest.request('/data.lua', 'POST', options, false, false, form2)
+  if (response2.error) return response
+
+  //return response2.body
 }
 
 /**
@@ -59,48 +103,24 @@ const form2 = {
  * @param  {Object} options - FritzBox.js options object.
  * @return {Array} Returns an array of active calls.
  */
-fritzInet.setNeverStandardAccess = async (options) => {
+fritzInet.getProfileList = async (options) => {
   if (!options.sid) {
     options.sid = await fritzLogin.getSessionId(options)
     if (options.sid.error) return options.sid
   }
-
   options.removeSidFromUri = true
-  const form1 = {
-    sid: options.sid,
-    back_to_page:'/internet/kids_profilelist.lua',
-    edit:'filtprof1',
-    time:'never',
-     netappschosen: '',
-    choosenetapps:'choose',
-    validate:'apply',
-    xhr:1
-  }
-  const response = await fritzRequest.request('/internet/kids_profileedit.lua', 'POST', options, false, false, form1)
-  if (response.error) return response
 
-const form2 = {
+  const form2 = {
     sid: options.sid,
-    back_to_page:'/internet/kids_profilelist.lua',
-    edit:'filtprof1',
-    time:'never',
-    oldpage:'/internet/kids_profileedit.lua',
-    lang:'en',
-    timer_complete:1,
-    no_sidrenew:'',
-    apply:'',
-     netappschosen: '',
-    choosenetapps:'choose',
-    xhr:1
+    page: 'kidPro',
+    lang: 'en',
+    no_sidrenew: '',
+    xhr: 1
   }
   const response2 = await fritzRequest.request('/data.lua', 'POST', options, false, false, form2)
   if (response2.error) return response
   //const result = JSON.parse(response.body).data.foncalls.activecalls
   return response2.body
 }
-
-
-
-// Export fritzFon.
 
 module.exports = fritzInet
